81d06f70fff42a206f34dead72b8990f79d6a14c
A. Kotelnikov <kotelnikov@enhancelab.ru>
2020-05-19 02:13:29 +0300

    docs: update readme; add description for endpoints

--- api/routes.py
+++ api/routes.py
@@ -13,6 +13,8 @@ router = APIRouter()
 @router.post("/users", response_model=UserProfileSchema)
 async def create_user(user_data: UserCreateSchema,
                       db=Depends(get_db)):
+    """Регистрация нового пользователя;
+    """
     user = UserService(db).create_user(**user_data.dict())
     return user
 
@@ -20,12 +22,18 @@ async def create_user(user_data: UserCreateSchema,
 @router.get("/users/{user_id}", response_model=UserProfileSchema)
 async def get_user_profile(user_id: int,
                            db=Depends(get_db)):
+    """Получение профиля пользователя.
+    Передавать все поля модели пользователя плюс ĸоличество рецептов, созданных пользователем;
+    """
     user = UserService(db).get_user(id_=user_id)
     return user
 
 
 @router.get("/recipes", response_model=List[RecipeSchema])
 async def get_recipes(filters: RecipeFilter = Depends(), db=Depends(get_db)):
+    """Получение списĸа рецептов;
+    Фильтрация рецептов по: части названия, типу блюда, хештегу;
+    """
     recipes = RecipeService(db).get_list(**filters.dict(exclude_unset=True))
     print(recipes)
     return recipes
@@ -33,5 +41,8 @@ async def get_recipes(filters: RecipeFilter = Depends(), db=Depends(get_db)):
 
 @router.get("/recipes/top", response_model=List[RecipeSchema])
 async def get_recipes_top(limit: int = 10, db=Depends(get_db)):
+    """Получение ТОП-а рецептов (больше лайĸов, выше место в рейтинге).
+    Количество рецептов в выдаче передается параметром `limit`.
+    """
     recipes = RecipeService(db).get_top(limit)
     return recipes
--- readme.md
+++ readme.md
@@ -1,8 +1,26 @@
 #Recipe service
 
+##global req
+1. Python 3.8+
+2. PostgeSQL 10+
+
 ##install
 1. clone this repo
-2. pip install -r requirements.txt
-3. python run.py
+2. Install dependencies
+    ```
+    pip install -r requirements.txt
+    ```
+3. *[optional]* run 
+    ```shell script
+    python initial_data.py
+    ```
+   for create DB schema and load demo fixtures
+4. Start server:
+    ```shell script
+    python run.py
+    ```
 
- 
+##API Docs
+Swagger documentation are available there: http://localhost:8000/api/docs#/
+
+
